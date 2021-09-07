cdeb9ff3322586ae010f1f7d97e57e2d8da895ec
yamamotohirofumi <sun.lionking.88@gmail.com>
2021-09-07 05:35:36 +0000

    user機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :posts, dependent: :destroy
+  attachment :profile_image
+
+  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :introduction, length: {maximum: 100}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
+  resources :users, only: [:index, :show, :edit, :update]
 end

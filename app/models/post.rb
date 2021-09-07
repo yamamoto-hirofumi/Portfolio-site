428bee121fb8c12bb18b7d35ce277379b7d48231
yamamotohirofumi <sun.lionking.88@gmail.com>
2021-09-07 08:40:11 +0000

    validates、エラー文追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   attachment :image
+  validates :title, length: { maximum: 50 }, presence: true
+  validates :content, length: { maximum: 150 }, presence: true
 end

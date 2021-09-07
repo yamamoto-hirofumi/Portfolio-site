95c723c9a3a050ee33e546506fb083d92c4d9502
yamamotohirofumi <sun.lionking.88@gmail.com>
2021-09-07 11:38:08 +0000

    コメント機能追加/非同期はまだ

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
   attachment :image
   validates :title, length: { maximum: 50 }, presence: true
   validates :content, length: { maximum: 150 }, presence: true

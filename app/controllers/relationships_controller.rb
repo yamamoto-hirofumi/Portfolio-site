d7227921fc11c045bcf902d5e18a6b54bb3cd60e
yamamotohirofumi <sun.lionking.88@gmail.com>
2021-09-09 12:18:41 +0000

    非同期通信

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,20 +1,38 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
-    current_user.follow(params[:user_id])
-    redirect_to request.referer
+    #@user = User.find(params[:followed_id])
+    #binding.pry
+    #urrent_user.follow(params[:user_id])
+    @user = current_user.follow(params[:user_id])
+    # follow = @user.new(user_id: current_user.id)
+
+    #@user.save
+    # current_user.follow(params[:user_id])
+
+    # @book = Book.find(params[:book_id])
+    # favorite = @book.favorites.new(user_id: current_user.id)
+    # favorite.save
+
+
+
+    #redirect_to request.referer
   end
-  
+
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
-  end 
-  
+    #redirect_to request.referer
+  end
+
   def followings
+    #binding.pry
     user = User.find(params[:user_id])
     @users = user.followings
   end
-  
+
   def followers
+    #binding.pry
     user = User.find(params[:user_id])
     @users = user.followers
   end
--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 2021_09_08_025851) do
     t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|

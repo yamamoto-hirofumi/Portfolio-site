6f3fd9570b275c670730c53dbd9cd168aacdf31a
yamamotohirofumi <sun.lionking.88@gmail.com>
2021-09-18 14:19:26 +0000

    連続ログインでflashメッセージが出る機能追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def login_history_text
+  def login_history_image
     if current_user.login_histories.count < 10
       image_tag("login_count_image3", size: '100x40')
     elsif current_user.login_histories.count < 20
@@ -7,14 +7,6 @@ module ApplicationHelper
     else
       image_tag("login_count_image1", size: '100x40')
     end
-  end
-  # ログインしたらカウント上がる
-  def continuous_login
-      while current_user.login_histories.count == 30 do
-        if  user_signed_in?
-          current_user.login_histories.count + 1
-        end
-      end
   end
 end
 

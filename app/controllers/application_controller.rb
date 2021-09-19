a27fc472e7fb12190216019a82e346eaddc22d57
yamamotohirofumi <sun.lionking.88@gmail.com>
2021-09-19 12:26:16 +0000

    flashのエラー解決

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :count_login_history
-
-  before_action :continuous_login
 
 
   def after_sign_in_path_for(resource)
@@ -14,47 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   private
-
-    # ログインしたらカウント上がる
-  def continuous_login
-    if current_user
-      first_day = ""
-      current_user.login_histories.order(logind_at: "desc").each_with_index do |login_history, index|
-       if current_user.login_histories.order(logind_at: "desc").last.logind_at.to_date == login_history.logind_at.to_date
-         first_day = login_history.logind_at.to_date
-         break
-       end
-        if (login_history.logind_at.to_date - current_user.login_histories.order(logind_at: "desc")[index+1].logind_at.to_date).to_int > 1
-          first_day = login_history.logind_at.to_date
-          break
-        end
-      end
-
-      num = 1
-      current_user.login_histories.where(logind_at: first_day..current_user.login_histories.order(logind_at: "asc").last.logind_at).order(logind_at: "asc").each_with_index do |login_history, index|
-        if current_user.login_histories.order(logind_at: "asc").last.logind_at.to_date == login_history.logind_at.to_date
-         break
-        end
-        if (current_user.login_histories.order(logind_at: "asc")[index+1].logind_at.to_date - login_history.logind_at.to_date).to_int == 1
-           num += 1
-        end
-      end
-      flash.now[:alert]  = num.to_s + "日連続ログインおめでとうございます！！"
-    end
-  end
-
-  # ログイン記録を作成する
-  def count_login_history
-    if current_user
-      if current_user.login_histories.present?
-        unless current_user.login_histories.order(logind_at: "asc").last.logind_at.to_date == Date.today
-          current_user.login_histories.create(logind_at: DateTime.now)
-        end
-      else
-        current_user.login_histories.create(logind_at: DateTime.now)
-      end
-    end
-  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  after_action :count_login_history
+
+  after_action :continuous_login
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -24,4 +27,45 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  private
+    # ログインしたらカウント上がる
+  def continuous_login
+    if current_user
+      first_day = ""
+      current_user.login_histories.order(logind_at: "desc").each_with_index do |login_history, index|
+       if current_user.login_histories.order(logind_at: "desc").last.logind_at.to_date == login_history.logind_at.to_date
+         first_day = login_history.logind_at.to_date
+         break
+       end
+        if (login_history.logind_at.to_date - current_user.login_histories.order(logind_at: "desc")[index+1].logind_at.to_date).to_int > 1
+          first_day = login_history.logind_at.to_date
+          break
+        end
+      end
+
+      num = 1
+      current_user.login_histories.where(logind_at: first_day..current_user.login_histories.order(logind_at: "asc").last.logind_at).order(logind_at: "asc").each_with_index do |login_history, index|
+        if current_user.login_histories.order(logind_at: "asc").last.logind_at.to_date == login_history.logind_at.to_date
+         break
+        end
+        if (current_user.login_histories.order(logind_at: "asc")[index+1].logind_at.to_date - login_history.logind_at.to_date).to_int == 1
+           num += 1
+        end
+      end
+      flash[:notice]  = num.to_s + "日連続ログインおめでとうございます！！"
+    end
+  end
+
+  # ログイン記録を作成する
+  def count_login_history
+    if current_user
+      if current_user.login_histories.present?
+        unless current_user.login_histories.order(logind_at: "asc").last.logind_at.to_date == Date.today
+          current_user.login_histories.create(logind_at: DateTime.now)
+        end
+      else
+        current_user.login_histories.create(logind_at: DateTime.now)
+      end
+    end
+  end
 end

9ea98bafc371e6ecdcdb2703dd2558f7c74474bf
yamamotohirofumi <sun.lionking.88@gmail.com>
2021-09-19 07:11:06 +0000

    image更新

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def login_history_image
     if current_user.login_histories.count < 10
-      image_tag(asset_path("login_count_image3"), size: '100x40')
+      image_tag(asset_path("login_count_image3.jpeg"), size: '100x40')
     elsif current_user.login_histories.count < 20
-      image_tag(asset_path("login_count_image2"), size: '100x40')
+      image_tag(asset_path("login_count_image2.jpeg"), size: '100x40')
     else
-      image_tag(asset_path("login_count_image1"), size: '100x40')
+      image_tag(asset_path("login_count_image1.jpeg"), size: '100x40')
     end
   end
 end

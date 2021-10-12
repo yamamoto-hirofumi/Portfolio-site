bfd9ed34f9264523543c023092a71ee698aab3c3
yamamotohirofumi <sun.lionking.88@gmail.com>
2021-10-12 01:27:00 +0000

    ログインカウント特典修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   # ログインカウントに応じ画像変更
   def login_history_image
-    if current_user.login_histories.count < 10
+    if User.find(params[:id]).login_histories.count < 10
       image_tag(asset_path("login_count_image3.jpeg"), size: '100x40')
-    elsif current_user.login_histories.count < 20
+    elsif User.find(params[:id]).login_histories.count < 20
       image_tag(asset_path("login_count_image2.jpeg"), size: '100x40')
     else
       image_tag(asset_path("login_count_image1.jpeg"), size: '100x40')
